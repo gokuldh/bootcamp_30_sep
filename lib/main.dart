050edd7e094b85bfc4235570a499695e49b1dbab
Vardaan Sharma <gokuldh@gmail.com>
2020-10-02 22:30:29 +0530

    Day 3

--- lib/main.dart
+++ lib/main.dart
@@ -13,16 +13,25 @@ class FlutterBootcamp extends StatelessWidget{
        appBar: AppBar(
          title: Text(Strings.titletext),
          backgroundColor: Colors.deepOrange,
+         centerTitle: true,
        ),
-       body: Center(
-         child: Text(
-           Strings.bodytext,
-           style: TextStyle(
-             color: Colors.deepOrange,
-             fontSize: 30
-           ),
+       body: Container(
+         color: Colors.grey,
+         child: Text("SKILLTO"),
+         padding: EdgeInsets.fromLTRB(30, 15, 30, 15),
+         margin: EdgeInsets.fromLTRB(100, 100, 0, 0),
+       ),
+       floatingActionButton: FloatingActionButton(
+         child: Icon(
+           Icons.alternate_email,
+           size: 30,
+           color: Colors.white,
          ),
-       ),
+         backgroundColor: Colors.deepOrange,
+         onPressed: () {
+           print("I was Clicked");
+         },
+       ) ,
      ),
    );
   }
